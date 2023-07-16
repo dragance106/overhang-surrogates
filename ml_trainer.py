7f7b1ddc13e7ef41bfd80ab92f692e856dbe8bd3
Marko Milosevic <ninja643@gmail.com>
2023-07-16 12:24:39 +0200

    Final dnn training

--- ml_trainer.py
+++ ml_trainer.py
@@ -5,15 +5,16 @@ import xgboost as xgb
 import datetime
 import math
 import multiprocessing as mp
+import os
 
 from mipt import mipt, mipt_full, LHS_maximin
 
 sample_size = 100   # the number of samples to be selected
-repeat_times = 10   # the number of ML models to be trained for each parameter combination,
+repeat_times = 5    # the number of ML models to be trained for each parameter combination,
                     # keeping only their average cvrmse as the final result
-num_models = 6      # the number of distinct ML models used in training
+num_models = 3      # the number of distinct ML models used in training
                     # at the moment, they are:
-                    # dnn8-10-1, dnn8551, dnn84441, dnn843331, dnn8333331, dnn83332221,
+                    # dnn881, dnn8631, dnn843331
                     # xgb-early10-lr0.3, xgb-early10-lr0.1, xgb-early10-lr0.03
 num_folds = 5       # number of folds for cross validation
 
@@ -55,7 +56,7 @@ def train_dnn_fold(load, train_folds, test_folds, neurons):
             min_delta=0.001,
             patience=10,        # number of epochs to wait before stopping training
             restore_best_weights=True,
-            start_from_epoch=10
+            # start_from_epoch=10
         )
 
         # fit the model
@@ -117,12 +118,12 @@ def train_models(df_training, load):
     all_trained_models = []
 
     # dnn-based models with dropout and batch normalization
-    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[10]))
-    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[5, 5]))
-    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[4, 4, 4]))
+    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[8]))
+    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[6, 3]))
+    # all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[4, 4, 4]))
     all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[4, 3, 3, 3]))
-    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[3, 3, 3, 3, 3]))
-    all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[3, 3, 3, 2, 2, 2]))
+    # all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[3, 3, 3, 3, 3]))
+    # all_trained_models.append(train_dnn_fold(load, train_folds, test_folds, neurons=[3, 3, 3, 2, 2, 2]))
 
     # xgboost-based models (already trained, so commented!)
     # all_trained_models.append(train_xgb_fold(load, train_folds, test_folds, early_stopping_rounds=10, learning_rate=0.3))
@@ -258,15 +259,14 @@ def process_this_combination(params):
 def generate_params(df):
     # pass through all combinations of the office cell model parameters,
     # apart from the overhang depth and height
-    for climate in [4]: # range(6):
-        for obstacle in [0]: # range(5):
-            for orientation in [0.0]: # [0.0, 45.0, -45.0]:
-                for heat_SP in [21]: # [19, 21]:
-                    for cool_SP in [24]: # [24, 26]:
-                        for load in ['heat_load [kWh/m2]', 'cool_load [kWh/m2]', 'light_load [kWh/m2]']:
-                                    # ['heat_load [kWh/m2]', 'cool_load [kWh/m2]', 'light_load [kWh/m2]', 'primary [kWh/m2]']:
-                            for sampling_method_name in ['mipt_full']: # ['LHS_maximin', 'mipt', 'mipt_full']:
-                                for num_inputs in [8]: # [2, 8]:
+    for climate in range(6):
+        for obstacle in range(5):
+            for orientation in [0.0, 45.0, -45.0]:
+                for heat_SP in [19, 21]:
+                    for cool_SP in [24, 26]:
+                        for load in ['heat_load [kWh/m2]', 'cool_load [kWh/m2]', 'light_load [kWh/m2]', 'primary [kWh/m2]']:
+                            for sampling_method_name in ['LHS_maximin', 'mipt', 'mipt_full']:
+                                for num_inputs in [2, 8]:
                                     yield(df, climate, obstacle, orientation, heat_SP, cool_SP,
                                           load, sampling_method_name, num_inputs)
 
@@ -290,7 +290,7 @@ def process_all_combinations(df):
     df_all_cvrmse_results = pd.DataFrame.from_records(data=all_cvrmse_results,
                                                       columns=['climate', 'obstacle', 'orientation', 'heat_SP', 'cool_SP',
                                                                'load', 'sampling_method', 'num_inputs',
-                                                               'dnn8101', 'dnn8551', 'dnn84441', 'dnn843331', 'dnn8333331', 'dnn83332221',
+                                                               'dnn881', 'dnn8631', 'dnn843331',
                                                                #'xgb_lr0.3', 'xgb_lr0.1', 'xgb_lr0.03',
                                                                ])
     timestamp = datetime.datetime.now().strftime('%y_%m_%d_%H_%M_%S')
@@ -299,6 +299,7 @@ def process_all_combinations(df):
 
 
 if __name__=="__main__":
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
     df = pd.read_csv('collected_results.csv')
     mp.freeze_support()
     process_all_combinations(df)
